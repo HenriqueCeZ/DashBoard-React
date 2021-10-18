d9632b177848e3d2f3b3049a966eab8359161d99
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-18 11:09:26 -0300

    DummyData and Chart component is ready

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './home.css'
 import Featured from '../../components/featuredInfo/Featured'
+import Chart from '../../components/chart/Chart'
+import {userData} from '../../DummyData.js'; 
 
 export default function Home() {
     return (
         <div className='home'>
-                <Featured/>         
+                <Featured/> 
+                <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>        
         </div>
     )
 }
